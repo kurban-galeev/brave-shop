0603f5b8915cdaaf03323ca6e2961c3e7f256be2
kurban-galeev <kurban.galeev@mail.ru>
2021-12-24 11:31:08 +0400

    add and setting eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello man</h1>
-    </div>
-  );
-}
+const App = function (): JSX.Element {
+  return <h1> Hello man </h1>;
+};
 
 export default App;
